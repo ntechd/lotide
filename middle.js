1aba13c425e547102003c76d5a56de56c6340f22
Najma Duale <najmaduale1@gmail.com>
2021-11-30 07:06:41 +0000

    middle

--- middle.js
+++ middle.js
@@ -1,4 +1,5 @@
 // TEST/ASSERTION FUNCTIONS
+/*
 const eqArrays = function(x) {
 	//...
 }
@@ -6,19 +7,23 @@ const eqArrays = function(x) {
 const assertArraysEqual = function(actual, expected) {
 	//...
 }
-
+*/
 // ACTUAL FUNCTION
 const middle = function(array) {
 	if (array.length % 2 === 0){ // even
 			var x = array.length / 2
-			return console.log(array[x - 1],array[x] )
+			return [array[x - 1],array[x]]
 	} 
 	else
 	{
 			var x = array.length / 2
-			return console.log(array[x + 0.5 - 1])
+			return [array[x + 0.5 - 1]]
 	}
 }
 
+/*
 middle([1,2,3,4])
 middle([1,2,3,4,5])
+*/
+
+module.exports = middle;
