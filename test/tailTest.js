f6505ed63500ad019cb132db2b38c3aafadf9bfb
Najma Duale <najmaduale1@gmail.com>
2021-11-30 15:53:25 +0000

    reforactor test files

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,3 +1,4 @@
+/*
 const assertEqual = require("../assertEqual");
 const tail = require("../tail.js");
 const words = ["Yo Yo", "Lighthouse", "Labs"];
@@ -5,3 +6,20 @@ console.log(assertEqual(tail(words).length,words.length-1)); // Checking our fin
 console.log(assertEqual(words.length, 3)); // Checking original array
 
 console.log(assertEqual(tail([]).length,0));
+*/
+
+const assert = require('chai').assert;
+const tail = require('../tail');
+
+describe("#tail",()=>{
+	it("returns ['Lighthouse','Labs'] for ['Yo Yo','Lighthouse','Labs']",()=>{
+		assert.deepEqual(tail(['Yo Yo','Lighthouse','Labs']),['Lighthouse','Labs'])
+	});
+	it("returns [] for ['Yo Yo']",()=>{
+		assert.deepEqual(tail(['Yo Yo']),[])
+	});
+	it("returns [] for []",()=>{
+		assert.deepEqual(tail([]),[])
+	});
+
+});
