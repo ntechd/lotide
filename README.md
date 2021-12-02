920917fecdbdfb23e795e0050f4fb45945b650ed
Najma Duale <najmaduale1@gmail.com>
2021-12-02 01:55:36 +0000

    correting

---
+++ README.md
@@ -0,0 +1,32 @@
+# Lotide
+
+A mini clone of the [Lodash](https://lodash.com) library.
+
+## Purpose
+
+**_BEWARE:_ This library was published for learning purposes. It is _not_ intended for use in production-grade software.**
+
+This project was created and published by me as part of my learnings at Lighthouse Labs. 
+
+## Usage
+
+**Install it:**
+
+`npm install @najnpm/lotide`
+
+
+**Require it:**
+
+`const _ = require('@najnpm/lotide');`
+
+**Call it:**
+
+`const results = _.tail([1, 2, 3]) // => [2, 3]`
+
+## Documentation
+
+The following functions are currently implemented:
+
+* `head()`: this functions take the array and returns the head of the arry, and that is the first element
+* `tail()`: this function takes the array and returns all the elements except the head element
+* `middle()`: this function take the array and returns the middle element of the array
--- index.js
+++ index.js
@@ -3,6 +3,17 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const countLetters = require('./countLetters');
+const countOnly = require('.countonly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
 
 /*
 const assertArraysEqual = require('./assertArraysEqual');
@@ -15,6 +26,16 @@ module.exports = {
   head:   head,
   tail:   tail,
   middle: middle,
+	countLetters: countLetters,
+	countOnly: countOnly,
+	eqArrays: eqArrays,
+	eqObjects: eqObjects,
+	findKeyByValue: findKeyByValue,
+	findKey: findKey,
+	letterPositions: letterPositions,
+	map: map,
+	takeUntil: takeUntil,
+	without: without,
 	/*
 	assertArraysEqual: assertArraysEqual,
 	assertEqual: assertEqual,
@@ -22,4 +43,11 @@ module.exports = {
 	countLetters: countLetters,
 */
 };
+
+//shorthand
+module.exports = {
+  head,
+  tail,
+  middle
+}; 
 
